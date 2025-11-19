33aa6f80ac733b9e64473e953efc232157db20a1
LisNat <rivera.liseth@correounivalle.edu.co>
2025-11-18 22:28:37 -0500

    Cambio perspectiva de cámara y función saltar del player aplicada

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -5,23 +5,41 @@ public class MovimientoJugador : MonoBehaviour
     public CharacterController controller;
     public float velocidad = 6f;
     public float gravedad = -9.81f;
+    public float alturaSalto = 1.5f;
 
     Vector3 velocidadCaida;
+    bool estaEnSuelo;
 
     // Update is called once per frame
     void Update()
     {
-        // 1. Leer las teclas (W, A, S, D)
+        // Verificar si estamos tocando el piso
+        estaEnSuelo = controller.isGrounded;
+
+        // Si tocamos el suelo y la velocidad de caída es baja, la reseteamos
+        if (estaEnSuelo && velocidadCaida.y < 0)
+        {
+            velocidadCaida.y = -2f; // Pequeña fuerza para asegurar que toca el suelo
+        }
+
+        // Leer las teclas (W, A, S, D)
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // 2. Calcular la dirección hacia donde queremos ir
+        // Calcular la dirección hacia donde queremos ir
         Vector3 movimiento = transform.right * x + transform.forward * z;
 
-        // 3. Mover al personaje
+        // Mover al personaje
         controller.Move(movimiento * velocidad * Time.deltaTime);
 
-        // 4. Aplicar gravedad (para que no flote)
+        // SALTO: Solo si está en el suelo y presiona Espacio
+        if (Input.GetButtonDown("Jump") && estaEnSuelo)
+        {
+            // Fórmula física para saltar la altura exacta
+            velocidadCaida.y = Mathf.Sqrt(alturaSalto * -2f * gravedad);
+        }
+
+        // Aplicar gravedad (para que no flote)
         velocidadCaida.y += gravedad * Time.deltaTime;
         controller.Move(velocidadCaida * Time.deltaTime);
     }
