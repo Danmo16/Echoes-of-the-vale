d86ad1724892e98c7f24fa8d17065d487ef66fc0
LisNat <rivera.liseth@correounivalle.edu.co>
2025-12-08 14:32:16 -0500

    Nuevo script para el movimiento del player

---
+++ Assets/Scripts/LogicaPlayer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LogicaPleyer : MonoBehaviour
+{
+    public float velocidadMovimiento = 5.0f;
+    public float velocidadRotacion = 200.0f;
+    private Animator anim;
+    public float x, y; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Leer las teclas (W, A, S, D)
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        // Rotación y movimiento
+        transform.Rotate(0, x * velocidadRotacion * Time.deltaTime, 0);
+        transform.Translate(0, 0, y * velocidadMovimiento * Time.deltaTime);
+    }
+}
